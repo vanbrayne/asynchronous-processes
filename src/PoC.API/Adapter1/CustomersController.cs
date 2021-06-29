bdb54ef588f771a93616eec3256378bccea4650d
Lars Lindberg <lars.lindberg@xlent.se>
2021-06-29 16:10:51 +0200

    Added a convenience API method for testing purposes.

--- src/PoC.API/Adapter1/CustomersController.cs
+++ src/PoC.API/Adapter1/CustomersController.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Nexus.Link.Libraries.Core.Assert;
 using Nexus.Link.Libraries.Crud.Interfaces;
 using Nexus.Link.Libraries.Web.AspNet.Pipe.RespondAsync;
 using PoC.Example.Abstract.Capabilities.Common;
@@ -11,7 +13,7 @@ using PoC.Example.Abstract.Capabilities.CustomerOnboardingMgmt;
 namespace PoC.API.Adapter1
 {
     [ApiController]
-    [Route("Customer")]
+    [Route("Customers")]
     public class CustomersController : ControllerBase, ICreateAndReturn<Person, string>
     {
         private readonly ICustomerOnboardingMgmt _capability;
@@ -24,9 +26,34 @@ namespace PoC.API.Adapter1
         /// <inheritdoc />
         [HttpPost("")]
         [RespondAsync(RespondAsyncOpinionEnum.Always)]
-        public Task<Person> CreateAndReturnAsync(Person item, CancellationToken cancellationToken = default)
+        public async Task<Person> CreateAndReturnAsync(Person person, CancellationToken cancellationToken = default)
         {
-            return _capability.Customer.CreateAndReturnAsync(item, cancellationToken);
+            var result = await _capability.Customer.CreateAndReturnAsync(person, cancellationToken);
+            return result;
+        }
+
+        [HttpGet("Tests/{number}")]
+        [RespondAsync(RespondAsyncOpinionEnum.Always)]
+        public async Task<Person> PreparedTest(int number, CancellationToken cancellationToken = default)
+        {
+            ServiceContract.RequireGreaterThanOrEqualTo(1, number, nameof(number));
+            ServiceContract.RequireLessThanOrEqualTo(1, number, nameof(number));
+            switch (number)
+            {
+                case 1:
+                    {
+                        var person = new Person
+                        {
+                            EmailAddress = "jane.doe@example.com",
+                            PersonalNumber = "990529-0707"
+                        };
+                        var result = await _capability.Customer.CreateAndReturnAsync(person, cancellationToken);
+                        return result;
+                    }
+                default:
+                    FulcrumAssert.Fail($"Unexpected test number: {number}");
+                    throw new ArgumentException();
+            }
         }
     }
 }
